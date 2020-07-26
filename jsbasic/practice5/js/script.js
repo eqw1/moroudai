623e7e08e98df0bed515a278810a05642dbda683
eqw1 <eqwi93@gmail.com>
2020-07-26 18:25:54 +0300

    Практика 6. Работа с событиями

--- jsbasic/practice5/js/script.js
+++ jsbasic/practice5/js/script.js
@@ -1,45 +1,63 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
+/*jshint -W097 */
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    let moviesList = document.querySelector('.promo__interactive-list');
+    
+    function refreshUserFilmList() {
+        movieDB.movies.sort();
+        moviesList.innerHTML = "";
+        movieDB.movies.forEach((item, i) => {
+        moviesList.insertAdjacentHTML('beforeend', 
+                                      `<li class="promo__interactive-item">${i+1}. ${item}
+                                          <div class="delete"></div>
+                                       </li>`);
+        });
+        // Ивент удаления фильма из базы и со страницы при клике навешивается на все кнопки delete
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener("click", (event) => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                refreshUserFilmList();
+            });
+        });
+    }
+    
+    // Добавление новых просмотренных фильмов через форму без обновления страницы
+    // Обрезание фильмов длиннее 21 символа
+    let form = document.querySelector('form.add'),
+        input = form.querySelector('input'),
+        checkbox = form.querySelector('[type="checkbox"]');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+ 
+        let userFilm;
+        userFilm = input.value;
+        if (userFilm) {
+            if (userFilm.length >21) {
+                userFilm = userFilm.substr(0, 21) + "...";
+            }
+            movieDB.movies.push(userFilm);
+            if (checkbox.checked) {
+                console.log("Любимый фильм");
+            }
+            refreshUserFilmList();
+            event.target.reset(); 
+        }
+    });
 
-// 1
-document.querySelector('.promo__adv').remove();
-
-// 2
-document.querySelector('.promo__genre').textContent = "ДРАМА";
-
-// 3
-document.querySelector('.promo__bg').style.backgroundImage = 'url(../practice5/img/bg.jpg)';
-
-// 4-5
-let moviesList = document.querySelector('.promo__interactive-list');
-moviesList.innerHTML = "";
-movieDB.movies.sort();
-movieDB.movies.forEach((item, i) => {
-    moviesList.insertAdjacentHTML('beforeend', 
-                                  `<li class="promo__interactive-item">${i+1}. ${item}
-                                      <div class="delete"></div>
-                                   </li>`);
+    document.querySelector('.promo__adv').remove();
+    document.querySelector('.promo__genre').textContent = "ДРАМА";
+    document.querySelector('.promo__bg').style.backgroundImage = 'url(../practice5/img/bg.jpg)';
+    refreshUserFilmList();
 });
